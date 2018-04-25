c1adc7b5f56b1a4a4dee6d59e2aa59b7c8989d9d
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-04-25 15:29:23 +0700

    Initial Implementation

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "product_domain" {
+  type        = "string"
+  description = "The name of the product domain which this ASG belongs to"
+}
+
+variable "asg_name" {
+  type        = "string"
+  description = "The name of the auto scaling group"
+}
